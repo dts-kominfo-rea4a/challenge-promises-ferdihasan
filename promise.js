291b28af9503f01303141db9f1a92efd93ae839e
Ferdi Hasan <ferdihasanpwd@gmail.com>
2022-09-19 13:05:55 +0700

    update task

--- promise.js
+++ promise.js
@@ -1,8 +1,46 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
 
+const promiseOutput = emosi => {
+    let bioskopMarah = 0;
+    let bioskopTidakMarah = 0;
+    if (emosi === 'marah') {
+        return promiseTheaterIXX().then((value) => {
+            value.forEach((array) => {
+                if (array.hasil === emosi) {
+                    bioskopMarah++;
+                }
+            });
+            return promiseTheaterVGC();
+        }).then(value => {
+            value.forEach(array => {
+                if (array.hasil === emosi) {
+                    bioskopMarah++;
+                }
+            });
+            return bioskopMarah;
+        });
+
+    } else if (emosi === 'tidak marah') {
+        return promiseTheaterIXX().then(value => {
+            value.forEach(array => {
+                if (array.hasil === emosi) {
+                    bioskopTidakMarah++;
+                }
+            });
+            return promiseTheaterVGC();
+        }).then(value => {
+            value.forEach(array => {
+                if (array.hasil === emosi) {
+                    bioskopTidakMarah++;
+                }
+            });
+            return bioskopTidakMarah;
+        });
+    };
+
+}
 module.exports = {
-  promiseOutput,
-};
+    promiseOutput,
+}
